3cd2b74f740c5871e9440cb6f9c8beeea18ad167
Say1ddd <sajidmuhammadikhlas@gmail.com>
2026-07-19 20:29:46 +0700

    Fix unwanted string and raw object contatenation

--- src/utils/crypto/signer.ts
+++ src/utils/crypto/signer.ts
@@ -1,5 +1,11 @@
-import type { SignatureHeader } from '../../types.ts'
 import { hmacSha256, md5 } from './hash.ts'
+
+interface SignatureHeader {
+  platform: string
+  timestamp: string
+  dId: string
+  vName: string
+}
 
 export function createCredentialSignature(
   cred: string,
@@ -14,13 +20,15 @@ export function createApiSignature(
   timestamp: string,
 ): string {
   const header: SignatureHeader = {
-    dId: '',
     platform: '3',
-    vName: '1.0.0',
     timestamp,
+    dId: '',
+    vName: '1.0.0',
   }
 
-  const payload = path + timestamp + header
+  const headerJson = JSON.stringify(header)
+
+  const payload = path + timestamp + headerJson
 
   const hmac = hmacSha256(payload, salt)
 
